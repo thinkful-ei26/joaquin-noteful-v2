fc4c1d78a82f6a200ae88402bcf32a0f34401c37
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-22 13:51:20 -0700

    updated config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 'use strict';
 
 exports.PORT = process.env.PORT || 8080;
-
-TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/noteful-test';//should the last bit here be notes.test.js?
